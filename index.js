e7a17eb5a68360bb38c7aae1321fe4fab801902a
Raúl <ralo.dev@pm.me>
2024-01-17 10:08:22 -0600

    Corrección

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ alert("¡Error! Completa todos los campos");
 // Declara una variable llamada mensajeDeError y asígnale el valor "¡Error! Completa todos los campos". Ahora muestra una alerta con el valor de la variable mensajeDeError .
 let mensajeDeError = "¡Error! Completa todos los campos";
 // Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre .
-let nombreDeUsuario = prompt("¿Cuál es tu nombre?");
+nombre = prompt("¿Cuál es tu nombre?");
 // Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad.
-let edadDeUsuario = prompt("¿Cuál es tu edad?");
+edad = prompt("¿Cuál es tu edad?");
 // Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje "¡Puedes obtener tu licencia de conducir!".
-if (edadDeUsuario >= 18) {
+if (edad >= 18) {
   alert("¡Puedes obtener tu licencia de conducir!");
 }
